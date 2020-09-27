0d2e21176420698c15a1551be96dd2c0d9ac4c57
asrul10 <hanafi.asrul@kotaxdev.com>
2020-09-27 16:38:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,105 @@
+#!/usr/bin/env node
+"use strict";
+
+const amqp = require('amqplib');
+const ffmpeg = require('fluent-ffmpeg');
+const cliProgress = require('cli-progress');
+ 
+const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+const args = process.argv.slice(2);
+const queue = "Hello";
+
+// Publisher
+const publisher = async (fileName) => {
+  try {
+    const conn = await amqp.connect('amqp://localhost');
+    const ch = await conn.createChannel();
+
+    ch.assertQueue(queue, {
+      durable: false
+    });
+    ch.sendToQueue(queue, Buffer.from(JSON.stringify({ fileName: fileName, time: Date.now() })));
+    console.log(" [✔] Message sent!");
+
+    setTimeout(() => {
+      conn.close();
+      process.exit(0);
+    }, 500);
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Consumer
+const consumer = async () => {
+  try {
+    const conn = await amqp.connect('amqp://localhost');
+    const ch = await conn.createChannel();
+
+    ch.assertQueue(queue, {
+      durable: false
+    });
+    ch.prefetch(1);
+
+    console.log(" [⏳] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+    ch.consume(queue, async (msg) => {
+      try {
+        const message = JSON.parse(msg.content);
+        await convert(message.fileName);
+        ch.ack(msg);
+        console.log(" [✔] Finish converting!", message);
+      } catch (error) {
+        throw error;
+      }
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+const convert = (fileName) => {
+  bar1.start(100, 0);
+
+  return new Promise(function (resolve, reject) {
+    ffmpeg('source/' + fileName)
+      .output('dist/' + fileName + '-480p.mp4')
+      .audioCodec('copy')
+      .size('720x480')
+
+      .output('dist/' + fileName + '-720p.mp4')
+      .audioCodec('copy')
+      .size('1280x720')
+
+      .output('dist/' + fileName + '-1080p.mp4')
+      .audioCodec('copy')
+      .size('1920x1080')
+
+      .on('progress', function(progress) {
+        bar1.update(Math.round(progress.percent));
+      })
+      .on('error', function (err) {
+        reject(err);
+      })
+      .on('end', function () {
+        bar1.stop();
+        resolve("Success converted!");
+      })
+      .run();
+  });
+}
+
+if (args[0] === "publisher") {
+  if (typeof args[1] === "undefined") {
+    console.log("file name required!");
+    return;
+  }
+  publisher(args[1]);
+  return;
+}
+if (args[0] === "consumer") {
+  consumer();
+  return;
+}
+
+console.log("Invalid arguments!\nAvailable arguments:\n  - publisher\n  - consumer");
